b07fee30d5eaa05aeeeeb82b6dde8d94f75e67e3
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-09-02 16:26:19 -0500

    add home component, promotion service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { PromotionService} from './services/promotion.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -44,8 +46,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
-  bootstrap: [AppComponent]
+  providers: [ DishService, PromotionService ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -10,7 +10,16 @@ export class DishService {
 
   constructor() { }
 
-  getDishes():Dish[] {
+  getDishes(): Dish[] {
     return DISHES
   }
+
+  getDish(id: number):Dish[] {
+    return DISHES.filter((dish) => (dish.id === id))
+  }
+
+  getFeaturedDish():Dish {
+    return DISHES.filter( (dish) => dish.featured)[0];
+  }
+
 }
