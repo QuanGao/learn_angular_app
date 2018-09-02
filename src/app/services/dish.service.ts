f98f584a8ab2e1192dae574c57063f7dc717aeb7
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-09-02 17:58:14 -0500

    pass param in url

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -14,8 +14,8 @@ export class DishService {
     return DISHES
   }
 
-  getDish(id: number):Dish[] {
-    return DISHES.filter((dish) => (dish.id === id))
+  getDish(id: number):Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0]
   }
 
   getFeaturedDish():Dish {
