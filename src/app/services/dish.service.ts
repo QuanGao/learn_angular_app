9591864092a412fa95ce5d7fdb6219b8048a766a
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-09-16 13:27:24 -0500

    RxJS Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -12,14 +12,18 @@ export class DishService {
   constructor() { }
 
   getDishes(): Observable<Dish[]> {
-    return of(DISHES).pipe(delay(2000));
+    return of(DISHES).pipe(delay(1000));;
   }
 
   getDish(id: number):Observable<Dish> {
-    return of(DISHES.filter(dish => dish.id === id)[0]).pipe(delay(2000));
+    return of(DISHES.filter(dish => dish.id === id)[0]).pipe(delay(1000));
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return of(DISHES.filter(dish=>dish.featured)[0]).pipe(delay(2000));
+    return of(DISHES.filter(dish=>dish.featured)[0]).pipe(delay(1000));
+  }
+
+  getDishIds(): Observable<number[] | any> {
+    return of(DISHES.map(dish => dish.id));
   }
 }
